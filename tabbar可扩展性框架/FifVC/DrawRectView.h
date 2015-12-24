a5d7fcc9153f259f89581efd47e80636fa2bb6a5
zzxcc <314708369@qq.com>
2015-12-24 12:20:46 +0800

    Removed file that shouldn't be tracked

---
+++ tabbar可扩展性框架/FifVC/DrawRectView.h
@@ -0,0 +1,13 @@
+//
+//  DrawRectView.h
+//  tabbar可扩展性框架
+//
+//  Created by zzxcc on 15/11/26.
+//  Copyright © 2015年 Vutumn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrawRectView : UIView
+
+@end
---
+++ tabbar可扩展性框架/FifVC/DrawRectView.m
@@ -0,0 +1,13 @@
+//
+//  DrawRectView.m
+//  tabbar可扩展性框架
+//
+//  Created by zzxcc on 15/11/26.
+//  Copyright © 2015年 Vutumn. All rights reserved.
+//
+
+#import "DrawRectView.h"
+
+@implementation DrawRectView
+
+@end
